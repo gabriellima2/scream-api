ed7870c23b4fe9a8733d30a4e8c62dbda16e9dae
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-08-05 22:44:18 -0300

    style: change name of return base value constant

--- src/infra/helpers/scrapping/format-character-name.test.ts
+++ src/infra/helpers/scrapping/format-character-name.test.ts
@@ -1,11 +1,11 @@
 import { formatCharacterName } from "./format-character-name";
 
-const BASE_RETURN_VALUE = "any_value";
+const BASE_VALUE = "any_value";
 
 describe("FormatCharacterName function", () => {
 	it("should replace blank spaces with underscores", () => {
 		const returnedValue = formatCharacterName("any value");
-		expect(returnedValue).toBe(BASE_RETURN_VALUE);
+		expect(returnedValue).toBe(BASE_VALUE);
 	});
 	it("should remove invalid characters", () => {
 		const returnedValue = formatCharacterName("  any.value ");
@@ -13,10 +13,10 @@ describe("FormatCharacterName function", () => {
 	});
 	it("should change the chars to lower case", () => {
 		const returnedValue = formatCharacterName("ANY_VALUE");
-		expect(returnedValue).toBe(BASE_RETURN_VALUE);
+		expect(returnedValue).toBe(BASE_VALUE);
 	});
 	it("should return the formatted value", () => {
 		const returnedValue = formatCharacterName(" ANY VAL.UE   ");
-		expect(returnedValue).toBe(BASE_RETURN_VALUE);
+		expect(returnedValue).toBe(BASE_VALUE);
 	});
 });
--- src/infra/helpers/scrapping/format-overview-title.test.ts
+++ src/infra/helpers/scrapping/format-overview-title.test.ts
@@ -1,23 +1,23 @@
 import { formatOverviewTitle } from "./format-overview-title";
 
-const BASE_RETURN_VALUE = "any_value";
-const BASE_RETURN_VALUE_IN_PLURAL = "any_values";
+const BASE_VALUE = "any_value";
+const BASE_VALUE_IN_PLURAL = "any_values";
 
 describe("FormatOverviewTitle function", () => {
 	it("should replace blank spaces with underscores", () => {
 		const returnedValue = formatOverviewTitle("any value");
-		expect(returnedValue).toBe(BASE_RETURN_VALUE);
+		expect(returnedValue).toBe(BASE_VALUE);
 	});
 	it("should remove the parentheses by changing them to 's'", () => {
 		const returnedValue = formatOverviewTitle("any_value(s)");
-		expect(returnedValue).toBe(BASE_RETURN_VALUE_IN_PLURAL);
+		expect(returnedValue).toBe(BASE_VALUE_IN_PLURAL);
 	});
 	it("should change the chars to lower case", () => {
 		const returnedValue = formatOverviewTitle("ANY_VALUE");
-		expect(returnedValue).toBe(BASE_RETURN_VALUE);
+		expect(returnedValue).toBe(BASE_VALUE);
 	});
 	it("should format correctly", () => {
 		const returnedValue = formatOverviewTitle("ANY VALUE(S)");
-		expect(returnedValue).toBe(BASE_RETURN_VALUE_IN_PLURAL);
+		expect(returnedValue).toBe(BASE_VALUE_IN_PLURAL);
 	});
 });
