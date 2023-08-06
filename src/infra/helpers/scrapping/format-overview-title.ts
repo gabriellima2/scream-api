be29ba18f3316b61d45248d5b4477e55f4ffd57e
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-08-05 22:36:07 -0300

    test: add tests to format-overview-title helper function

--- src/infra/helpers/scrapping/format-overview-title.ts
+++ src/infra/helpers/scrapping/format-overview-title.ts
@@ -1,5 +1,8 @@
-import { BLANK_SPACES, BRACKETS } from "@/infra/constants/regex";
+import { BLANK_SPACES, PARENTHESES } from "@/infra/constants/regex";
 
 export function formatOverviewTitle(title: string) {
-	return title.replace(BLANK_SPACES, "_").replace(BRACKETS, "s").toLowerCase();
+	return title
+		.replace(BLANK_SPACES, "_")
+		.replace(PARENTHESES, "s")
+		.toLowerCase();
 }
