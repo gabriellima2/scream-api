3fb13e07a912fe90455cc29f178a3aff8ea501f8
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-08-24 16:29:37 -0300

    refactor: update movie model and schema adding @nestjs/mongoose features

--- src/infra/models/movie-model.ts
+++ src/infra/models/movie-model.ts
@@ -1,4 +1,79 @@
-import { model } from "mongoose";
-import { MovieSchema } from "../schemas/movie-schema";
+import { Document, Schema as MongooseSchema } from "mongoose";
+import { Prop, Schema } from "@nestjs/mongoose";
 
-export const MovieModel = model("movie", MovieSchema, "movies");
+import type { MovieOverview } from "@/domain/entities";
+
+@Schema()
+export class MovieModel extends Document {
+	@Prop({ type: String, required: true })
+	name: string;
+	@Prop({
+		type: String,
+		required: true,
+	})
+	image: string;
+	@Prop({
+		type: String,
+		required: true,
+	})
+	synopsis: string;
+	@Prop({
+		required: true,
+		type: {
+			directors: {
+				type: MongooseSchema.Types.Mixed,
+				required: true,
+				validate: {
+					validator: function (v) {
+						return (
+							typeof v === "string" ||
+							(Array.isArray(v) && v.every((item) => typeof item === "string"))
+						);
+					},
+					message: (props) =>
+						`${props.value} is not a valid value for myField!`,
+				},
+			},
+			writers: {
+				type: MongooseSchema.Types.Mixed,
+				required: true,
+				validate: {
+					validator: function (v) {
+						return (
+							typeof v === "string" ||
+							(Array.isArray(v) && v.every((item) => typeof item === "string"))
+						);
+					},
+					message: (props) =>
+						`${props.value} is not a valid value for myField!`,
+				},
+			},
+			producers: {
+				type: [String],
+				required: true,
+			},
+			composers: {
+				type: String,
+				required: true,
+			},
+			realease_date: {
+				type: String,
+				required: true,
+			},
+			running_time: {
+				type: String,
+				required: true,
+			},
+			box_office: {
+				type: String,
+				required: true,
+			},
+		},
+	})
+	overview: MovieOverview;
+	@Prop({
+		type: [String],
+		required: true,
+	})
+	characters: string[];
+}
--- src/infra/repositories/movie-repository.ts
+++ src/infra/repositories/movie-repository.ts
@@ -1,14 +1,18 @@
 import { ScreamInfoRepository } from "@/domain/repositories";
-import { MovieModel } from "../models/movie-model";
-import { Movie } from "@/domain/entities";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+
+import { MovieModel } from "../models";
+import type { Movie } from "@/domain/entities";
 
 export class MovieRepository implements ScreamInfoRepository<Movie> {
+	constructor(@InjectModel(MovieModel.name) private model: Model<MovieModel>) {}
 	async insert(data: Omit<Movie, "id">): Promise<Movie> {
-		const movie = await MovieModel.create<Movie>(data);
-		return Object.freeze({ ...movie, id: movie._id.toString() });
+		const movie = await this.model.create(data);
+		return Object.freeze({ ...movie, id: movie._id } as Movie);
 	}
 	async getByName(name: string): Promise<Movie> {
-		const movie = await MovieModel.findOne<Movie>({ name });
-		return Object.freeze(movie);
+		const movie = await this.model.findOne({ name }).exec();
+		return Object.freeze(movie as Movie);
 	}
 }
