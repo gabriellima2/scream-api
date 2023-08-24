af7371d7282fee9f06ebbcc4509afedbad124c99
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-08-24 16:59:51 -0300

    feat: add mongo connection structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+
+import { MovieSchema } from "./infra/schemas";
+import { MovieModel } from "./infra/models";
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			envFilePath: ".env",
 		}),
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: async (config: ConfigService) => ({
+				uri: config.get<string>("MONGO_URI"),
+			}),
+		}),
+		MongooseModule.forFeature([{ name: MovieModel.name, schema: MovieSchema }]),
 	],
 	controllers: [AppController],
 	providers: [AppService],
