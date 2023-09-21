a190848c1d3a11a723b63a962c202038fdfece8b
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-09-21 20:28:08 -0300

    fix: fixes movie name checking for scream-five

--- src/domain/helpers/functions/format-movie-name.ts
+++ src/domain/helpers/functions/format-movie-name.ts
@@ -1,19 +1,19 @@
 import { removeInvalidChars } from "./remove-invalid-chars";
 
-const names = [];
+let prevName = "";
 
 export function formatMovieName(name: string) {
 	const formattedName = removeInvalidChars(name);
-	if (names.includes("Scream 4") && formattedName === "Scream") {
+	if (prevName === "Scream 4" && formattedName === "Scream") {
 		const newName = "Scream 5";
-		names.push(newName);
+		prevName = newName;
 		return newName;
 	}
 	if (formattedName === "Scream VI") {
 		const newName = "Scream 6";
-		names.push(newName);
+		prevName = newName;
 		return newName;
 	}
-	names.push(formattedName);
+	prevName = formattedName;
 	return formattedName;
 }
