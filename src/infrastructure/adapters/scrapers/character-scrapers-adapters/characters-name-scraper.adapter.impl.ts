396cd6783df631a57774b705d924ea3b6dc143d3
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-10-06 16:49:04 -0300

    feat: adds name-entity to charaters-name-scraper

--- src/infrastructure/adapters/scrapers/character-scrapers-adapters/characters-name-scraper.adapter.impl.ts
+++ src/infrastructure/adapters/scrapers/character-scrapers-adapters/characters-name-scraper.adapter.impl.ts
@@ -2,8 +2,8 @@ import { CheerioAPI, load } from "cheerio";
 
 import { CharactersNameScraperAdapter } from "@/adapters/scrapers/character-scrapers-adapters/characters-name-scraper.adapter";
 import { CharactersNameScraperProtocols } from "@/core/domain/protocols/scrapers/character-scrapers.protocol";
+import { NameEntity } from "@/core/domain/entities/character-entity/name.entity";
 
-import { formatCharacterName } from "@/core/domain/functions/formatters/format-character-name";
 import { isEmptyArray } from "@/core/domain/functions/is-empty-array";
 
 export class CharactersNameScraperAdapterImpl
@@ -20,10 +20,11 @@ export class CharactersNameScraperAdapterImpl
 		if (!els) return undefined;
 		els.each((_, el) => {
 			$("ul > li", el).each((_, item) => {
-				const name = $(".category-page__member-link", item).text();
-				const hasInvalidChars = name.includes(":") || name.includes("(");
-				if (!name || hasInvalidChars) return;
-				names.push(formatCharacterName(name));
+				const name = NameEntity.create(
+					$(".category-page__member-link", item).text()
+				).value;
+				if (!name) return;
+				names.push(name);
 			});
 		});
 		return isEmptyArray(names) ? undefined : names;
