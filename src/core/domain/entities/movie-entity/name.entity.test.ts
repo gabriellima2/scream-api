1956b7d5a0aa7676340694be71bc35e336aa1958
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-10-06 18:49:12 -0300

    refactor: updates name-entity adding validate test

--- src/core/domain/entities/movie-entity/name.entity.test.ts
+++ src/core/domain/entities/movie-entity/name.entity.test.ts
@@ -1,17 +1,25 @@
 import { NameEntity } from "./name.entity";
 
 describe("NameEntity function", () => {
-	it("should format the name", () => {
-		const name = NameEntity.create("scream_2");
-		expect(name.value).toBe("Scream 2");
+	describe("Format", () => {
+		it("should format the name", () => {
+			const name = NameEntity.create("scream_2");
+			expect(name.value).toBe("Scream 2");
+		});
+		it("should replace char 'vi' to number 5", () => {
+			const name = NameEntity.create("Scream VI");
+			expect(name.value).toBe("Scream 6");
+		});
+		it("should add 5 when the current movie sequence is 5", () => {
+			NameEntity.create("Scream 4");
+			const name = NameEntity.create("Scream");
+			expect(name.value).toBe("Scream 5");
+		});
 	});
-	it("should replace char 'vi' to number 5", () => {
-		const name = NameEntity.create("Scream VI");
-		expect(name.value).toBe("Scream 6");
-	});
-	it("should add 5 when the current movie sequence is 5", () => {
-		NameEntity.create("Scream 4");
-		const name = NameEntity.create("Scream");
-		expect(name.value).toBe("Scream 5");
+	describe("Validate", () => {
+		it("should return undefined when passed a invalid value", () => {
+			const name = NameEntity.create("");
+			expect(name).toBeUndefined();
+		});
 	});
 });
