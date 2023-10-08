1dcf1b83f0e125ac3801a3786b11fc56f3f9ecbd
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-10-08 18:29:45 -0300

    test: removes repetitive tests from scraper-gateway-adapter-impl

--- src/infrastructure/adapters/gateways/scraper-gateway.adapter.impl.test.ts
+++ src/infrastructure/adapters/gateways/scraper-gateway.adapter.impl.test.ts
@@ -1,21 +1,14 @@
 import { ScraperGatewayAdapterImpl } from "./scraper-gateway.adapter.impl";
 
-import { EmptyDataException } from "@/core/domain/exceptions/empty-data.exception";
 import { HttpGatewayAdapter } from "@/adapters/gateways/http-gateway.adapter";
 import { BaseScraperAdapter } from "@/adapters/scrapers/base-scraper.adapter";
 
 import { characterHtml } from "@/__mocks__/character-html";
 
 type ExpectedScrapedData = { name: string };
-
 type Dependencies = {
 	http: HttpGatewayAdapter;
 	scraper: BaseScraperAdapter<ExpectedScrapedData>;
-};
-
-const BASE_URL = "any_url";
-const EXPECTED_SCRAPED_DATA: ExpectedScrapedData = {
-	name: "any_name",
 };
 
 const makeSut = (dependencies: Dependencies) => {
@@ -36,53 +29,54 @@ const mockDependenciesReturnValue = (
 };
 
 describe("ScraperGatewayAdapterImpl", () => {
-	describe("Methods", () => {
-		describe("Execute", () => {
-			describe("Success", () => {
-				it("should return the correct data from the scraped page", async () => {
+	const BASE_URL = "any_url";
+	const EXPECTED_SCRAPED_DATA: ExpectedScrapedData = {
+		name: "any_name",
+	};
+
+	describe("Success", () => {
+		it("should return the correct data from the scraped page", async () => {
+			const { http, scraper } = mockDependenciesReturnValue({
+				http: characterHtml.onlyName,
+				scraper: EXPECTED_SCRAPED_DATA,
+			});
+			const sut = makeSut({ http, scraper });
+
+			const result = await sut.execute(BASE_URL);
+
+			expect(result).toMatchObject(EXPECTED_SCRAPED_DATA);
+			expect(http.getHtml).toHaveBeenCalledWith(BASE_URL);
+			expect(http.getHtml).toHaveBeenCalledTimes(1);
+			expect(scraper.execute).toHaveBeenCalledWith(characterHtml.onlyName);
+			expect(scraper.execute).toHaveBeenCalledTimes(1);
+		});
+	});
+	describe("Errors", () => {
+		const cases = [
+			{
+				mockedHttpReturnValue: undefined,
+				mockedScraperReturnValue: EXPECTED_SCRAPED_DATA,
+			},
+			{
+				mockedHttpReturnValue: characterHtml.onlyName,
+				mockedScraperReturnValue: { name: undefined },
+			},
+		];
+		test.each(cases)(
+			"should throw an error when scraper return invalid value",
+			async ({ mockedHttpReturnValue, mockedScraperReturnValue }) => {
+				try {
 					const { http, scraper } = mockDependenciesReturnValue({
-						http: characterHtml.onlyName,
-						scraper: EXPECTED_SCRAPED_DATA,
+						http: mockedHttpReturnValue,
+						scraper: mockedScraperReturnValue,
 					});
 					const sut = makeSut({ http, scraper });
 
-					const result = await sut.execute(BASE_URL);
-
-					expect(result).toMatchObject(EXPECTED_SCRAPED_DATA);
-					expect(http.getHtml).toHaveBeenCalledWith(BASE_URL);
-					expect(http.getHtml).toHaveBeenCalledTimes(1);
-					expect(scraper.execute).toHaveBeenCalledWith(characterHtml.onlyName);
-					expect(scraper.execute).toHaveBeenCalledTimes(1);
-				});
-			});
-			describe("Errors", () => {
-				it("should throw an error when scraper return invalid value", async () => {
-					try {
-						const { http, scraper } = mockDependenciesReturnValue({
-							http: undefined,
-							scraper: EXPECTED_SCRAPED_DATA,
-						});
-						const sut = makeSut({ http, scraper });
-
-						await sut.execute(BASE_URL);
-					} catch (err) {
-						expect(err).toBeInstanceOf(Error);
-					}
-				});
-				it("should throw an error when scraper return invalid value", async () => {
-					try {
-						const { http, scraper } = mockDependenciesReturnValue({
-							http: characterHtml.onlyName,
-							scraper: { name: undefined },
-						});
-						const sut = makeSut({ http, scraper });
-
-						await sut.execute(BASE_URL);
-					} catch (err) {
-						expect(err).toBeInstanceOf(EmptyDataException);
-					}
-				});
-			});
-		});
+					await sut.execute(BASE_URL);
+				} catch (err) {
+					expect(err).toBeInstanceOf(Error);
+				}
+			}
+		);
 	});
 });
